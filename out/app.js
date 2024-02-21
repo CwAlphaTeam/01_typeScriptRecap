f9011155408534085899a62d04cad267c32c3faf
ensar <ensarsagkal@gmail.com>
2024-02-21 15:05:10 +0300

    endOfFirstDay

--- out/app.js
+++ out/app.js
@@ -1,27 +1,10 @@
 "use strict";
-var AreaTypes;
-(function (AreaTypes) {
-    AreaTypes["BUILDING"] = "BUILDING";
-    AreaTypes["FLOOR"] = "FLOOR";
-    AreaTypes["ROOM"] = "ROOM";
-})(AreaTypes || (AreaTypes = {}));
-const bulb = {
-    id: 1,
-    name: 'Bulb 1',
-    area: AreaTypes.ROOM,
-    areaInfo: {
-        areaID: "a",
-        areaName: "asd",
-        postalCode: 45
-    }
-};
-if (bulb.area === AreaTypes.ROOM) {
-    console.log('Device is in room');
-}
-else if (AreaTypes.FLOOR) {
-    console.log('Device is in floor');
-}
-else {
-    console.log('Device is in building');
-}
-console.log(bulb.area);
+let numb = 5;
+numb = "ali";
+numb = true;
+let x;
+x = 55;
+x = [1, 8, 7];
+let y = numb;
+console.log(y);
+console.log(typeof (y));
--- src/app.ts
+++ src/app.ts
@@ -54,44 +54,66 @@
 // console.log(herhangibiri)
 
 
- interface Device {
-  id: number
-  name: string
-  info?: string
-  area: AreaTypes
-  areaInfo:AreaInfo
-}
+//  interface Device {
+//   id: number
+//   name: string
+//   info?: string
+//   area: AreaTypes
+//   areaInfo:AreaInfo
+// }
 
- enum AreaTypes {
-  BUILDING = 'BUILDING',
-  FLOOR = 'FLOOR',
-  ROOM = 'ROOM',
-}
+//  enum AreaTypes {
+//   BUILDING = 'BUILDING',
+//   FLOOR = 'FLOOR',
+//   ROOM = 'ROOM',
+// }
 
-const bulb: Device = {
-id: 1,
-name: 'Bulb 1',
-area: AreaTypes.ROOM,
-areaInfo:{
-    areaID:"a",
-    areaName:"asd",
-    postalCode:45
-}
-}
+// const bulb: Device = {
+// id: 1,
+// name: 'Bulb 1',
+// area: AreaTypes.ROOM,
+// areaInfo:{
+//     areaID:"a",
+//     areaName:"asd",
+//     postalCode:45
+// }
+// }
 
-if(bulb.area === AreaTypes.ROOM) {
-console.log('Device is in room')
-} else if (AreaTypes.FLOOR) {
-console.log('Device is in floor')
-} else {
-console.log('Device is in building')
-}
-interface AreaInfo{
+// if(bulb.area === AreaTypes.ROOM) {
+// console.log('Device is in room')
+// } else if (AreaTypes.FLOOR) {
+// console.log('Device is in floor')
+// } else {
+// console.log('Device is in building')
+// }
+// interface AreaInfo{
     
-        areaID:string,
-        areaName:string,
-        postalCode?:number
+//         areaID:string,
+//         areaName:string,
+//         postalCode?:number
 
-}
+// }
 
-console.log(bulb.area)
+// console.log(bulb.area)
+
+
+// //*any type
+// let numb:any=5
+// numb="ali"
+// numb=true
+
+// let x:any
+// x= 55
+// x=[1,8,7]
+
+// let y:string=numb
+// console.log(y)
+// console.log(typeof(y))
+
+
+// //*unknown type
+//  let xyz: unknown= 2023
+// xyz="2004"
+//  let asd: string=xyz,
+//  let bool:boolean=xyz
+//  let tip:any=xyz
