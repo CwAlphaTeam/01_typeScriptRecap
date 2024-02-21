aabb7f4131d0752f31a192b3eab98b2e846798bd
ensar <ensarsagkal@gmail.com>
2024-02-21 14:03:55 +0300

    recap1

---
+++ out/app.js
@@ -0,0 +1,4 @@
+"use strict";
+let ogrenciListesi = [[1, "ali", false]];
+ogrenciListesi.push([2, "ahmet", true]);
+console.log(ogrenciListesi);
---
+++ src/app.ts
@@ -0,0 +1,30 @@
+// console.log("hello world")
+// //abc
+
+// let num:number = 5
+// let ad: string= "ch15"
+
+// // num="ali"
+// //  ad= false
+
+
+//  let ogrenciNo:number[]=[];
+//  ogrenciNo.push(1946)
+//  console.log(ogrenciNo)
+// //  ogrenciNo.push("2024")
+ 
+// let ogrenciAd:string[]=[]
+// ogrenciAd.push("ali")
+// // ogrenciAd.push(false)
+
+// let tuple:[number,string,boolean]=[1,"ali",true]
+// tuple.push(3)
+// console.log(tuple)
+// tuple.push(false)
+// console.log(tuple)
+// tuple.push([])
+
+// let ogrenciListesi:[number,string,boolean][]=[[1,"ali",false]]
+// ogrenciListesi.push([2,"ahmet",true])
+// console.log(ogrenciListesi)
+// ogrenciListesi.push([3,"ayse",true])
