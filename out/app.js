2a8d3a9ea336a3192c330e4afe674cffd8c868d7
ensar <ensarsagkal@gmail.com>
2024-02-21 14:17:40 +0300

    enum

--- out/app.js
+++ out/app.js
@@ -1,4 +1,20 @@
 "use strict";
-let ogrenciListesi = [[1, "ali", false]];
-ogrenciListesi.push([2, "ahmet", true]);
-console.log(ogrenciListesi);
+var Roles;
+(function (Roles) {
+    Roles[Roles["user"] = 0] = "user";
+    Roles[Roles["admin"] = 1] = "admin";
+    Roles[Roles["guest"] = 2] = "guest";
+    Roles[Roles["other"] = 3] = "other";
+})(Roles || (Roles = {}));
+let currentUser = Roles.admin;
+console.log(currentUser);
+var Puanlar;
+(function (Puanlar) {
+    Puanlar[Puanlar["kaldi"] = 10] = "kaldi";
+    Puanlar[Puanlar["zorlaGecti"] = 15] = "zorlaGecti";
+    Puanlar[Puanlar["orta"] = 25] = "orta";
+    Puanlar[Puanlar["iyi"] = 50] = "iyi";
+    Puanlar[Puanlar["pekiyi"] = 80] = "pekiyi";
+})(Puanlar || (Puanlar = {}));
+let herhangibiri = Puanlar.iyi;
+console.log(herhangibiri);
